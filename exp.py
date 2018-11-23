227bc99dec90a0b2e10b4c541ceb15e58e1fe170
inbal zelinger <inbal.zelinger@live.biu.ac.il>
2018-11-24 00:01:27 +0200

    name_changed

--- exp.py
+++ exp.py
@@ -9,26 +9,15 @@ consumer_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx"
 access_token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXx"
 access_token_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
-# Creating the authentication object
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# Setting your access token and secret
 auth.set_access_token(access_token, access_token_secret)
-# Creating the API object while passing in auth information
-api = tweepy.API(auth)
 
-# Creating the API object while passing in auth information
 api = tweepy.API(auth)
-
-# The Twitter user who we want to get tweets from
 name = "realDonaldTrump"
 # Number of tweets to pull
-
 tweetCount = 1
 
-# Calling the user_timeline function with our parameters
 results = api.user_timeline(id=name, count=tweetCount)
 
-# foreach through all tweets pulled
 for tweet in results:
-   # printing the text stored inside the tweet object
    print (tweet.text)
