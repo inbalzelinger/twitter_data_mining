71660b3d69520ca670e375865e09337e04834483
inbal zelinger <inbal.zelinger@live.biu.ac.il>
2018-11-24 22:30:49 +0200

    elastic_search_exp

--- exp.py
+++ exp.py
@@ -1,23 +1,35 @@
-"""
-Spyder Editor
-This is a temporary script file.
-"""
-
 import tweepy
-consumer_key = "XXXXXXXXXXXXXXXXXXXXX"
-consumer_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx"
-access_token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXx"
-access_token_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+import json
+import requests
+from elasticsearch import Elasticsearch
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+r = requests.get('http://localhost:9200')
+i = 1
+while r.status_code == 200:
+    r = requests.get('http://swapi.co/api/people/' + str(i))
+    es.index(index='sw', doc_type='people', id=i, body=json.loads(r.content))
+    i = i + 1
 
-api = tweepy.API(auth)
-name = "realDonaldTrump"
-# Number of tweets to pull
-tweetCount = 1
+print(i)
+print(es.get(index='sw', doc_type='people', id=5))
 
-results = api.user_timeline(id=name, count=tweetCount)
 
-for tweet in results:
-   print (tweet.text)
+
+# consumer_key = "XXXXXXXXXXXXXXXXXXXXX"
+# consumer_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx"
+# access_token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXx"
+# access_token_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
+#
+# api = tweepy.API(auth)
+# name = "realDonaldTrump"
+# # Number of tweets to pull
+# tweetCount = 1
+#
+# results = api.user_timeline(id=name, count=tweetCount)
+#
+# for tweet in results:
+#    print (tweet.text)
