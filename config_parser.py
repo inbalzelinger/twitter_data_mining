fabda3f3e40f1cbb3d9d977dc34cb774ed1b966f
inbal zelinger <inbal.zelinger@live.biu.ac.il>
2018-11-26 21:51:29 +0200

    twitter data to elastic

--- config_parser.py
+++ config_parser.py
@@ -1,15 +1,17 @@
 from backports import configparser
 
 ##TODO change this there is no reason to read all the config in the condtructor.
+##TODO maybe change this to yml file>?
 class Config_parser:
-    def __init__(self):
+    def __init__(self, configuration_file_path):
+        self.file_path = configuration_file_path
         self.twitter_for_developers_config = self.__read_config('TWITTER')
         self.elastic_search_config = self.__read_config('ELASTICSEARCH')
 
     def __read_config(self, section):
         config_parser = configparser.RawConfigParser()
         ##TODO better to use built in relative path!!!
-        config_file_path = r'configuration\configuration.conf'
+        config_file_path = self.file_path
         config_parser.read(config_file_path)
         return config_parser[section]
 
