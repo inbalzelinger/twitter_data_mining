0b93cf1fbc06ccacae8ddd5f89f129f894411c2c
hadar sabag <hadar.sabag@live.biu.ac.il>
2018-12-18 16:52:04 +0200

    query tests

---
+++ Menu.py
@@ -0,0 +1,17 @@
+
+class menu:
+
+    def __init__(self):
+        self.l = 'l'
+
+    def key_words(self):
+        user_input = input("enter your search key words\n")
+        return user_input
+
+    def location(self):
+        user_input = input("aggregate by:\n")
+        return user_input
+
+    def aggregate_by (self):
+        user_input = input("aggregate by:\n")
+        return user_input
