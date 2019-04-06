3b905760abde3599f5915a3346087634aa425b24
inbal zelinger <inbal.zelinger@live.biu.ac.il>
2019-04-06 16:33:42 +0300

    kmeans

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from elasticsearch import Elasticsearch
 from tweepy import OAuthHandler
 from tweepy import Stream
 import Menu
+from ml_algo.kmeans import kmeans_algo
 from stream_api import StreamApi
 from config_parser import Config_parser
 from elasticsearch_dsl import Search
 import ArabiziCheck
 
-MAX_RESULTS = 100000
+MAX_RESULTS = 200000
 INDEX_NAME = "twitter_index"
 DOC_TYPE = "twitter"
+TWITTER_TIMEOUT = 1
 
 def main():
     conf_dictionary = read_configuration_file(r'configuration/configuration.conf')
@@ -22,15 +24,15 @@ def main():
     quary_filters = menu.aggregate_by()
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
     es.indices.create(index= INDEX_NAME, ignore=400)
-    # kmeans_algo(es, 7, location)
+    es.indices.create(index= "arabizi_index", ignore=400)
     listener = StreamApi(es)
-    stream = Stream(auth=auth, listener=listener, timeout=1)
+    stream = Stream(auth=auth, listener=listener, timeout=TWITTER_TIMEOUT)
     try:
         stream.filter(locations=location)
     except Exception as e:
         print(e)
     createAggregation(quary_filters,es)
-    #create_user_locartion_dict(es)
+    kmeans_algo(es, 6)
     arabiziChecker = ArabiziCheck.arabiziChecker()
     check_arabizi_from_und(es,arabiziChecker)
 
@@ -42,10 +44,11 @@ def createQuery(quary_filters, es, index_name = INDEX_NAME):
         print("{0}) {1}".format(doc['_id'], doc['_source']['content']))
 
 
-def createAggregation(field,es, index_name = INDEX_NAME):
-    ##TODO enable the field first.
-    #es.put_mapping(doc_type='twitter_index', body='properties: {lang: {type: text,fielddata: true}}')
-    s = Search(using=es, index = index_name, doc_type="twitter")
+def createAggregation(field, es, index_name = INDEX_NAME):
+    ##enable the field (nessecary if its a test field
+    es.indices.put_mapping(doc_type=DOC_TYPE, index= INDEX_NAME,
+                           body={"properties": {"user.lang": { "type":"text","fielddata": "true"}}})
+    s = Search(using=es, index = INDEX_NAME, doc_type=DOC_TYPE)
     s.aggs.bucket('by_lang', 'terms', field=field)
     t = s.execute()
     print(t.aggregations.by_lang.buckets)
@@ -56,14 +59,14 @@ def createAggregation(field,es, index_name = INDEX_NAME):
 def check_arabizi_from_und(es,arabiziChecker, language = 'en'):
     sum_of_arabizi = 0
     arabizi_tweets = []
-    es.indices.put_settings(index='twitter_index',body={"index": {"max_result_window": MAX_RESULTS}})
-    res = es.search(index=INDEX_NAME, doc_type= DOC_TYPE, body={"size":MAX_RESULTS,"query": {"match_all":{}}})
+    es.indices.put_settings(index=INDEX_NAME,body={"index": {"max_result_window": MAX_RESULTS}})
+    res = es.search(index=INDEX_NAME, doc_type= DOC_TYPE,
+                    body={"size":MAX_RESULTS,"query": {"match_all":{}}})
     print("{0} {1} tweets found".format(res['hits']['total'], language))
     i=0
     for tweet in res['hits']['hits']:
         i+=1
         if tweet["_source"] is not None:
-            #print("{}(text)s".format(tweet["_source"]))
             if 'text' in tweet["_source"]:
                 if arabiziChecker.checkTweet(tweet["_source"]['text']):
                     arabizi_tweets.append(tweet["_source"]['text'])
@@ -73,7 +76,7 @@ def check_arabizi_from_und(es,arabiziChecker, language = 'en'):
     with open('configuration\output.txt', 'w', encoding='UTF-8') as output_file:
         for tweet in arabizi_tweets:
             output_file.write(tweet)
-        output_file.write("tweets number = {}".format(i))
+        output_file.write("\ntweets number = {}".format(i))
 
 
 def read_configuration_file(conf_file_path):
@@ -87,11 +90,13 @@ def read_configuration_file(conf_file_path):
 
 def create_user_locartion_dict(es):
     i = 0
-    res = es.search(index=INDEX_NAME, doc_type=DOC_TYPE, body={"size":MAX_RESULTS,"query": {"match_all":{}}})
+    res = es.search(index=INDEX_NAME, doc_type=DOC_TYPE,
+                    body={"size":MAX_RESULTS,"query": {"match_all":{}}})
     for tweet in res['hits']['hits']:
         if 'user.location' in tweet["_source"]:
             i += 1
     print(i)
+
 
 if __name__ == '__main__':
     main()
